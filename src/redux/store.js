04209d84a2c0e18e83f2808f6a66e37c269b7f1f
Sahil Ranjan Mund <msahilranjan@gmail.com>
2021-05-20 15:14:54 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,21 @@
+// import { createStore,applyMiddleware,compose} from "redux";
+// import {composeWithDevTools} from 'redux-devtools-extension';
+// import thunk from 'redux-thunk'
+// import rootReducer from './reducer';
+
+// const store = createStore(
+//   rootReducer,compose(applyMiddleware(thunk),
+//   composeWithDevTools()) 
+// );
+
+// export default store;
+
+
 import { createStore,applyMiddleware,compose} from "redux";
-import {composeWithDevTools} from 'redux-devtools-extension';
+
 import thunk from 'redux-thunk'
 import rootReducer from './reducer';
 
-const store = createStore(
-  rootReducer,compose(applyMiddleware(thunk),
-  composeWithDevTools()) 
-);
+const store = createStore(rootReducer,applyMiddleware(thunk));
 
 export default store;
